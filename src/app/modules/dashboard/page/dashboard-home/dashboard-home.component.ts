feaf013bcf48b9be10d4a73d0daf14089ef17764
Italo Cruz <italocruz544@gmail.com>
2025-01-28 09:16:21 -0300

    feat: prevent memory leak

--- src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
+++ src/app/modules/dashboard/page/dashboard-home/dashboard-home.component.ts
@@ -1,16 +1,17 @@
 import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
 import { MessageService } from 'primeng/api';
 import { ProductsService } from './../../../../services/products/products.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
   styleUrls: []
 })
-export class DashboardHomeComponent implements OnInit{
-
+export class DashboardHomeComponent implements OnInit, OnDestroy{
+  private destroy$ = new Subject<void>()
   public productsList: Array<GetAllProductsResponse> = [];
 
   constructor(
@@ -25,6 +26,9 @@ export class DashboardHomeComponent implements OnInit{
 
   getProductsDatas(): void {
     this.ProductsService.getAllProducts()
+    .pipe(
+      takeUntil(this.destroy$)
+    )
     .subscribe({
       next: (response) => {
         if(response.length > 0){
@@ -41,5 +45,10 @@ export class DashboardHomeComponent implements OnInit{
         })
       }
     })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,18 +1,21 @@
 import { MessageService } from 'primeng/api';
 import { AuthRequest } from './../../models/interfaces/user/auth/AuthRequest';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { CookieService } from 'ngx-cookie-service';
 import { SignupUserRequest } from 'src/app/models/interfaces/user/signupUserRequest';
 import { UserService } from 'src/app/services/user/user.service';
 import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy{
+
+  private destroy$ = new Subject<void>();
   loginCard = true;
 
   loginForm = this.formBuilder.group({
@@ -38,6 +41,9 @@ export class HomeComponent {
   onSubmitLoginForm(): void{
     if(this.loginForm.value && this.loginForm.valid){
       this.userService.authUser(this.loginForm.value as AuthRequest)
+      .pipe(
+        takeUntil(this.destroy$)
+      )
       .subscribe({
         next: (response) => {
           if(response){
@@ -68,6 +74,9 @@ export class HomeComponent {
     if(this.signupForm.value && this.signupForm.valid){
       this.userService
       .signupUser(this.signupForm.value as SignupUserRequest)
+      .pipe(
+        takeUntil(this.destroy$)
+      )
       .subscribe({
         next: (response) => {
           if(response){
@@ -91,5 +100,10 @@ export class HomeComponent {
         }
       })
     }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
